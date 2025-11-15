c22e391da73ddb6a3573ea8a24fb470e175adb6e
Serj-Game <greyf6235@gmail.com>
2025-11-16 01:50:32 +0300

    Добавил форму регистрации с отправкой в файл txt

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const path = require('path');
+const fs = require("fs")
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello  !');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/req-data', (req, res) => {
+  console.log(">>>>>>>>>>>>>>>>>>>", req.body)
+  fs.appendFile("data.txt", JSON.stringify(req.body) + "\n", (err)=>{
+    if(err) {
+      res.status(500).send("User not addad")
+    } else {
+      res.status(201).send("User addad")
+    }
+  })
 });
 
 app.listen(port, () => {
